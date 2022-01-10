2ef00787994606275510b8401d3bc7cb3183a062
ParkJunHyuk <akznsk1@gmail.com>
2022-01-10 16:57:13 +0900

    update 1932

--- DP/1932번_정수삼각형.py
+++ DP/1932번_정수삼각형.py
@@ -6,7 +6,6 @@ for i in range(1,n+1):
     triangle_list.append(list(map(int,input().split())))
     dp = [[0] * i for i in range(1,n+1)]
     dp[0][0] = triangle_list[0][0]
-print(dp[1][1])
 
 for j in range(1,n):
     for k in range(j+1):
