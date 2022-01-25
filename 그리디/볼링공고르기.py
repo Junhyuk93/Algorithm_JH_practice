9855f6f656f3d0215ae719261a98a89b34cec0d0
ParkJunHyuk <akznsk1@gmail.com>
2022-01-25 23:42:11 +0600

    update greedy

---
+++ 그리디/볼링공고르기.py
@@ -0,0 +1,37 @@
+# 무지성 2중 for문 방법
+n, m = map(int,input().split())
+
+weight = list(map(int,input().split()))
+print(weight)
+
+answer = 0
+
+for i in range(n):
+    for j in range(i,n):
+        if weight[i] != weight[j]:
+            answer += 1
+
+print(answer)
+
+
+#############################################
+
+
+# 답지 해설
+n, m = map(int,input().split())
+weight = list(map(int,input().split()))
+
+array = [0] * 11
+
+for i in weight:
+    array[i] += 1
+
+result = 0
+
+# 1부터 m까지의 각 무게에 대하여 처리
+for i in range(1, m+1):
+    # 무게가 i 인 볼링공의 개수(A가 선택할 수 있는 개수) 제외
+    n -= array[i]
+    # B가 선택하는 경우의 수와 곱
+    result += array[i] * n
+print(result)
