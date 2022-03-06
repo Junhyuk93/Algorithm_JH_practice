e62f4599c6f4dc1b33eb09e8bbb0838ed2c84714
Junhyuk93 <akznsk1@gmail.com>
2022-03-06 13:55:35 +0900

    update 문자열 재정렬

---
+++ 구현/문자열 재정렬.py
@@ -0,0 +1,19 @@
+import re
+n = input()
+n= list(n)
+
+num_list = []
+text_list =[]
+for i in range(len(n)):
+    if str.isdigit(n[i]) == True:
+        num_list.append(int(n[i]))
+    else:
+        text_list.append(n[i])
+
+num_list = sum(num_list)
+
+text_list.sort()
+
+text = ''.join(text_list)
+answer = text + str(num_list)
+print(answer)
